87c70a80d86cc6d6d7f30f3ef96795de39795641
lxhoi <lxhoi.2k@gmail.com>
2025-06-26 21:13:18 +0700

    Update variable name to 'conga' and fix print statement

---
+++ declare.py
@@ -0,0 +1,4 @@
+my_first_name = "Le xuan hoi"
+fav_number = 7
+concac_dai = "15cm"
+print(f"Hello, {my_first_name}! Your favorite number is {fav_number}. Con cac dai is {concac_dai}.")
---
+++ hello.py
@@ -0,0 +1,6 @@
+name = "World"
+print(f"Hello, {name}!")
+
+# A simple loop
+for i in range(5):
+    print(f"Iteration {i}")
